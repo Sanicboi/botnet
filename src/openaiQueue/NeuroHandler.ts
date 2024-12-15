a276f1e131e6a613378299431eedc30cf858c75a
sanicboi <alexandrzezekalo@gmail.com>
2024-12-15 13:14:53 +0100

    Testing

--- src/openaiQueue/NeuroHandler.ts
+++ src/openaiQueue/NeuroHandler.ts
@@ -81,6 +81,7 @@ export class NeuroHandler {
         });
       } else if (j.task === "voice") {
         const file = await FileHandler.getFileContent(j.voiceUrl);
+        console.log(j.voiceUrl, file.type);
         const transcription = await openai.audio.transcriptions.create({
           file: file,
           model: "whisper-1",
