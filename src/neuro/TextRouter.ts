2da31654d34940af81efcd43ea753856f50d7648
sanicboi <alexandrzezekalo@gmail.com>
2024-12-26 23:19:22 +0300

    Testing

--- src/neuro/TextRouter.ts
+++ src/neuro/TextRouter.ts
@@ -127,6 +127,7 @@ export class TextRouter extends Router {
     }
 
     if (q.data!.startsWith("ac-")) {
+      console.log("Action")
       if (q.data!.endsWith("-asst_1BdIGF3mp94XvVfgS88fLIor")) {
         const asst = await Router.manager.findOneBy(Action, {
           id: q.data!.substring(3),
