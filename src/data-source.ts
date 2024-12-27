bffe7c2a6246ec5629730e964117df959e42b511
sanicboi <alexandrzezekalo@gmail.com>
2024-12-27 14:43:00 +0300

    New system

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,9 +6,6 @@ import { Thread } from "./entity/assistants/Thread";
 import { Action } from "./entity/assistants/Action";
 import { SupportRequest } from "./entity/SupportRequest";
 import { FileUpload } from "./entity/assistants/FileUpload";
-import { Bot } from "./entity/bots/Bot";
-import { Lead } from "./entity/bots/Lead";
-import { Dialog } from "./entity/bots/Dialog";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
