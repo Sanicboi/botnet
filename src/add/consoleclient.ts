247865a5769cb15b9d3c2a1e74b307a0945414cb
sanicboi <alexandrzezekalo@gmail.com>
2025-01-12 19:48:33 +0100

    gram client

---
+++ src/add/consoleclient.ts
@@ -0,0 +1,39 @@
+//@ts-ignore
+import input from 'input';
+import { Api, TelegramClient } from 'telegram';
+require("dotenv").config();
+
+(async () => {
+    const token = await input.text("Введите токен:");
+    const client = new TelegramClient(token, 
+        +process.env.TG_API_ID!,
+        process.env.TG_API_HASH!,
+        {
+            useWSS: true
+        }
+    );
+
+    await client.connect();
+
+    let command = '';
+    while (command !== 'quit') {
+        
+        if (command === "join") {
+            const chat = await input.text("Channel or Chat name:");
+            await client.invoke(new Api.channels.JoinChannel({
+                channel: chat
+            }));
+        }
+
+        if (command === "dialogs") {
+            const dialogs = await client.getDialogs();
+            for (const dialog of dialogs) {
+                console.log(`${dialog.entity?.className} ${dialog.entity?.id.toJSON()}`)
+            }
+        }
+
+        
+
+        command = await input.text("Command:");
+    }
+})();
