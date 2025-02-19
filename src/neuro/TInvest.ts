d7f329e106fb9297231555f9e47fa461789d26b7
sanicboi <alexandrzezekalo@gmail.com>
2025-02-19 21:25:51 +0100

    investments

--- src/neuro/TInvest.ts
+++ src/neuro/TInvest.ts
@@ -1,10 +1,48 @@
 import axios from "axios";
+import {TinkoffInvestApi} from "tinkoff-invest-api";
+import { InstrumentIdType } from "tinkoff-invest-api/cjs/generated/instruments";
+import { CandleInterval, LastPriceType } from "tinkoff-invest-api/cjs/generated/marketdata";
+import { openai } from ".";
 
+const api = new TinkoffInvestApi({
+    token: process.env.T_INVEST_TOKEN!
+})
 export class TInvest {
 
 
     public static async getAnalysis(ticker: string): Promise<string> {
-        const res = await axios.get(`http://invest:6667/api/active/${ticker}`);
-        return res.data;
+        const bond = await api.instruments.bondBy({
+            idType: InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER,
+            id: ticker
+        })
+        
+        const candles = await api.marketdata.getCandles({
+            instrumentId: bond.instrument?.uid,
+            interval: CandleInterval.CANDLE_INTERVAL_DAY,
+        });
+
+        //@ts-ignore
+        const lastPrices = await api.marketdata.getLastPrices({
+            instrumentId: [
+                bond.instrument?.uid!
+            ],
+            lastPriceType: LastPriceType.LAST_PRICE_EXCHANGE,
+        });
+
+        const result = await openai.chat.completions.create({
+            messages: [
+                {
+                    role: 'developer',
+                    content: 'Ты - инвестор. Твоя задача - проанализировать данные о свечах и последних ценах акции. Сделай максимально подробный отчет.',
+                },
+                {
+                    role: 'user',
+                    content: `Свечи: ${JSON.stringify(candles.candles)}\n\nПоследняя цена: ${JSON.stringify(lastPrices.lastPrices)}`
+                }
+            ],
+            model: 'gpt-4o-mini'
+        });
+
+        return result.choices[0].message.content!;
     }
 }
