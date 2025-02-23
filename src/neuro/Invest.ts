9275cf66033aa0603f089380b1f574ec9c0a8cdb
sanicboi <alexandrzezekalo@gmail.com>
2025-02-23 20:49:16 +0100

    Crypto new

--- src/neuro/Invest.ts
+++ src/neuro/Invest.ts
@@ -14,24 +14,10 @@ export class Invest {
                 'access_key': process.env.INVEST_TOKEN,
                 symbols: ticker,
                 'date_to': dayjs().format('YYYY-MM-DD'),
-                'date_from': dayjs().subtract(1, 'month').format('YYYY-MM-DD'),
+                'date_from': dayjs().subtract(1, 'week').format('YYYY-MM-DD'),
             }
         });
 
-        const result = await openai.chat.completions.create({
-            messages: [
-                {
-                    role: 'developer',
-                    content: 'Ты - инвестор. Твоя задача - проанализировать данные о акции. Сделай максимально подробный отчет.',
-                },
-                {
-                    role: 'user',
-                    content: `Последние цены: ${JSON.stringify(results.data)}`
-                }
-            ],
-            model: 'gpt-4o-mini'
-        });
-
-        return result.choices[0].message.content!;
+        return JSON.stringify(results.data);
     }
 }
