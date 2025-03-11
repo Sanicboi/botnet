87617e8425b26516484b5163f008578eaf30610a
Sanicboi <alexandrzezekalo@gmail.com>
2025-03-11 11:10:59 +0100

    Update login.ts

--- src/add/login.ts
+++ src/add/login.ts
@@ -1,6 +1,7 @@
 import { TelegramClient } from "telegram";
 import { StringSession } from "telegram/sessions";
 import { DataSource } from "typeorm";
+import { UserBot } from "../entity/bots/UserBot.ts";
 //@ts-ignore
 import input from "input";
 require("dotenv").config();
@@ -13,5 +14,16 @@ src.initilize().then(async () => {
   while (number != "q") {
     const session = new StringSession("");
     const client = new TelegramClient(session, +process.env.API_ID!, process.env.API_HASH!, {});
+
+    await client.start({
+      phoneNumber: number,
+      phoneCode: async () => await input.text("Code: "),
+      onError: async (err) => true
+    });
+
+    const b = new UserBot();
+    b.token = await client.session.save();
+    await src.manager.save(b);
+    await client.destroy();
   }
 })
